34ae5cca7809f3e81636469ed55984a527d403a7
Elena Tokumura <e.tokumura@kenpalinc.com>
2026-04-14 15:03:46 +0900

    fix: header height

--- src/Tiptap.tsx
+++ src/Tiptap.tsx
@@ -39,19 +39,23 @@ const Tiptap = () => {
 		<div className="flex gap-4 w-full items-stretch flex-1 min-h-0">
 			<span className="text-sm font-bold text-left">Tiptap</span>
 			<div className="flex-1 flex flex-col gap-1">
-				<span className="text-sm font-bold text-left">HTML</span>
+				<div className="flex items-center h-6">
+					<span className="text-sm font-bold text-left">HTML</span>
+				</div>
 				<pre className="flex-1 border border-gray-500 rounded-lg p-4 overflow-auto bg-white/10 text-left text-sm whitespace-pre-wrap break-all">
 					<code>{html}</code>
 				</pre>
 			</div>
 			<div className="flex-1 flex flex-col gap-1">
-				<span className="text-sm font-bold text-left">JSON</span>
+				<div className="flex items-center h-6">
+					<span className="text-sm font-bold text-left">JSON</span>
+				</div>
 				<pre className="flex-1 border border-gray-500 rounded-lg p-4 overflow-auto bg-white/10 text-left text-sm whitespace-pre-wrap break-all">
 					<code>{json}</code>
 				</pre>
 			</div>
 			<div className="flex-1 flex flex-col gap-1">
-				<div className="flex items-center justify-between gap-2">
+				<div className="flex items-center justify-between gap-2 h-6">
 					<span className="text-sm font-bold text-left">Editor</span>
 					<div>
 						<button
