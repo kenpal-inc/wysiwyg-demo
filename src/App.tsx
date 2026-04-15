aec3c10f46b26d1b3f861c1871799175662590d1
Elena Tokumura <e.tokumura@kenpalinc.com>
2026-04-15 10:39:14 +0900

    feat: add TinyMCE demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./index.css";
 import Quill from "./Quill";
+import TinyMCE from "./TinyMCE";
 import Tiptap from "./Tiptap";
 
 export function App() {
 	return (
 		<div className="w-full h-screen p-8 flex flex-col text-center relative z-10">
+			<TinyMCE />
+			<hr className="my-4 border-gray-500 shrink-0" />
 			<Quill />
 			<hr className="my-4 border-gray-500 shrink-0" />
 			<Tiptap />
---
+++ src/TinyMCE.tsx
@@ -0,0 +1,50 @@
+import { Editor } from "@tinymce/tinymce-react";
+import { useState } from "react";
+
+import "tinymce/tinymce";
+import "tinymce/models/dom/model";
+import "tinymce/themes/silver";
+import "tinymce/icons/default";
+import "tinymce/skins/ui/oxide/skin";
+import "tinymce/skins/content/default/content";
+
+const initialValue = `
+TinyMCE Editor demo
+<blockquote>
+あのイーハトーヴォのすきとおった風、<br>
+夏でも底に冷たさをもつ青いそら、<br>
+うつくしい森で飾られたモリーオ市、<br>
+郊外のぎらぎらひかる草の波。
+</blockquote>
+<a href="https://www.aozora.gr.jp/cards/000081/files/1935_19925.html">ポラーノの広場 - 宮沢賢治</a>
+`;
+
+export default function TinyMCE() {
+	const [html, setHtml] = useState("");
+
+	return (
+		<div className="flex gap-4 w-full items-stretch flex-1 min-h-0">
+			<span className="text-sm font-bold text-left w-14 shrink-0">TinyMCE</span>
+			<div className="flex-1 flex flex-col gap-1">
+				<div className="flex items-center h-6">
+					<span className="text-sm font-bold text-left">HTML</span>
+				</div>
+				<pre className="flex-1 border border-gray-500 rounded-lg p-4 overflow-auto bg-white/10 text-left text-sm whitespace-pre-wrap break-all">
+					<code>{html}</code>
+				</pre>
+			</div>
+			<div className="flex-1 flex flex-col gap-1">
+				<div className="flex items-center justify-between gap-2 h-6">
+					<span className="text-sm font-bold text-left">Editor</span>
+				</div>
+				<Editor
+					licenseKey="gpl"
+					initialValue={initialValue}
+					init={{ menubar: false, resize: false }}
+					onInit={(_evt, editor) => setHtml(editor.getBody().innerHTML)}
+					onEditorChange={(_cnt, editor) => setHtml(editor.getBody().innerHTML)}
+				/>
+			</div>
+		</div>
+	);
+}
